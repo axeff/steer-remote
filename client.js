4182fc7139606709b93d0c9a817f69fb0b920eee
Axel Freudiger <axel.freudiger@gmail.com>
2013-09-05 16:16:04 +0200

    added basic functionality node socket server

---
+++ client.js
@@ -0,0 +1,32 @@
+// if user is running mozilla then use it's built-in WebSocket
+window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+var connection = new WebSocket('ws://localhost:1337/',['peer-protocol']);
+
+$(function () {
+
+    connection.onopen = function () {
+        // connection is opened and ready to use
+        console.log("connection is opened and ready to use");
+        
+        // establish connection with 
+        connection.send(JSON.stringify({register:true}))
+    };
+
+    connection.onerror = function (error) {
+        // an error occurred when sending/receiving data
+        console.log("an error occurred when sending/receiving data",error);
+    };
+
+    connection.onmessage = function (message) {
+        // try to decode json (I assume that each message from server is json)
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+        // handle incoming message
+        console.log(json);
+    };
+});
